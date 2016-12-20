7d562e372b51783735f5e844d27d04aa9b29bb8d
black-dragon74 <nickk.2974@gmail.com>
2016-12-20 22:21:02 +0530

    Show changelogs with option to don't show again
    Signed-off-by: black-dragon74 <nickk.2974@gmail.com>

--- app/src/main/java/com/nick/santractor/WelcomeFragment.java
+++ app/src/main/java/com/nick/santractor/WelcomeFragment.java
@@ -2,16 +2,21 @@ package com.nick.santractor;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 
@@ -20,6 +25,7 @@ import android.widget.Toast;
  */
 public class WelcomeFragment extends Fragment {
 Activity mActivity = getActivity();
+static boolean canShowChangelog;
 
     public WelcomeFragment() {
         // Required empty public constructor
@@ -37,6 +43,9 @@ Activity mActivity = getActivity();
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        // Init Shared Preferences
+        final SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(mActivity);
 
         // Change toolbar title
         Toolbar toolbar = (Toolbar) mActivity.findViewById(R.id.toolbar);
@@ -56,6 +65,34 @@ Activity mActivity = getActivity();
                 drawer.openDrawer(GravityCompat.START);
             }
         });
+
+        // Show changelog
+        canShowChangelog =  mPref.getBoolean("show_changelog", true);
+        AlertDialog.Builder changeLog = new AlertDialog.Builder(mActivity);
+        View mChangelog = View.inflate(mActivity, R.layout.frame_checkbox, null);
+        final CheckBox cbDont = (CheckBox) mChangelog.findViewById(R.id.cbDont);
+        changeLog.setView(mChangelog);
+        changeLog.setTitle("Changelog");
+        changeLog.setMessage(getResources().getString(R.string.changelog));
+        changeLog.setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                boolean res;
+                if (cbDont.isChecked()){
+                    res = false;
+                }
+                else{
+                    res = true;
+                }
+                mPref.edit().putBoolean("show_changelog", res).apply();
+                dialogInterface.dismiss();
+            }
+        });
+        if (canShowChangelog) {
+            AlertDialog change = changeLog.create();
+            change.setCanceledOnTouchOutside(false);
+            change.show();
+        }
         return mView;
     }
 
